ef8d4f1423263fb77aeae8868505fa33d91ef018
MarshalJoe <joe.marshall@objectrocket.com>
2018-11-25 14:27:30 -0600

    Adds basic MeetupCard

--- components/MeetupCard.vue
+++ components/MeetupCard.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="meetup-card">
-    <h2 class="text-center">{{ title }}</h2>
-    <h3 class="text-left">{{ sizeMembers }} </h3>
-    <p class="text-left" v-html="description"></p>
+  <div class="text-left meetup-card">
+    <div class="meetup-card__header">
+      <div class="meetup-card__logo"></div>
+      <div class="meetup-card__header_text">
+          <h2 class="meetup-card__title">{{ title }}</h2>
+          <h3 class="meetup-card__size">{{ sizeMembers }} </h3>
+      </div>
+    </div>
+    <p v-html="description"></p>
     <div class="meetup-card__next-meetup">
       <p>{{ nextMeetup }}</p>
     </div>
@@ -49,5 +54,46 @@ export default {
 </script>
 
 <style>
+
+.meetup-card {
+  padding: 25px;
+  transition: 1s;  
+  border: 1px solid lightgrey;
+  cursor: pointer;
+}
+
+.meetup-card:hover {
+  box-shadow: 3px 3px 2px 1px rgba(0, 0, 0, 0.3);;
+}
+
+.meetup-card__header {
+  display: table;
+  padding:10px;
+}
+
+.meetup-card__logo {
+  height: 75px;
+  width: 75px;
+  margin-right: 10px;
+  background-image: url("/images/react-logo.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.meetup-card__header_text {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.meetup-card__size {
+  font-size:1rem;
+  color:gray;
+}
+
+.meetup-card__next-meetup {
+  font-size: .85rem;
+  text-transform: uppercase;
+  color: #3b5998;
+}
 
 </style>
